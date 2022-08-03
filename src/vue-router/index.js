0ccc3966269a8f42a0ea8824fd29793d38020727
戴鹏鹏 <16602124116@163.com>
2022-08-03 22:20:55 +0800

    commit

--- src/vue-router/index.js
+++ src/vue-router/index.js
@@ -15,3 +15,15 @@ export function createRouter(options) {
 export function createWebHistory() {}
 
 export function createWebHashHistory() {}
+
+
+// hash 值变化 不会重新加载页面 
+// hash 就是前端的锚点，监控锚点的变化，渲染对应的组件，他不会响服务端发起请求，不能做 SEO 优化
+// 通过 window.location.hash 跳转    通过 onhashchange 监听路由变化
+
+
+// history 当刷新的时候会向服务端发起请求资源。
+// 通过 window.history.pushState 跳转   通过 on('popstate') 监听浏览器前进后退
+
+
+// 通过 historyAPI 可以实现 hash 模式， 都采用 history 的方式来使用
