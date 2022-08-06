cb8ff81234d46f3775316418ddd090b94548b1cd
戴鹏鹏 <16602124116@163.com>
2022-08-06 21:36:35 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
-// import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
-import { createRouter, createWebHashHistory, createWebHistory } from '../vue-router/index'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHashHistory, createWebHistory } from '../vue-router/index'
 import HomePage from '../pages/home.vue'
 import AboutPage from '../pages/about.vue'
 
-// const HomePage = { template: '<div>Home</div>' }
-// const AboutPage = { template: '<div>About</div>' }
 
 
 const routes = [
